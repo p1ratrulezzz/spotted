c67e4272d90d891707ff7556a982a41d6133efb6
olnaz <olnazx@gmail.com>
2017-10-14 16:55:37 +0300

    Update example

--- examples/echo.js
+++ examples/echo.js
@@ -3,14 +3,14 @@
 const spotty = require('../main');
 
 spotty.addCommunity({
-  accessToken: '',
-  confirmationCode: '',
-  id: 123456789,
-  secretKey: ''
+  accessToken:      'aswewoqpfsqwepofdsf', // <String> VK Community Access Token
+  confirmationCode: 'asdjfdg',             // <String> VK Community Confirmation Code
+  id:               12345678,              // <Number> VK Community ID
+  secretKey:        'secretAJSDIFfsd'      // <String> VK Community Secret Key
 });
 
 spotty.on('message_new', ctx => {
-  console.log(`[Group id${ctx.group_id}] New message from user id${ctx.object.user_id} received:`, ctx.object.body);
+  console.log(`[Group id${ctx.group_id}] A new message from user id${ctx.object.user_id} received:`, ctx.object.body);
 
   ctx.reply('Re: ' + ctx.object.body);
 });
